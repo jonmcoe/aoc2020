d64902b33140b094c45ad24a9d883457aed574cb
Jon Coe <jonmcoe@gmail.com>
2020-12-02 15:52:12 -0500

    add day02

---
+++ src/Days/Day02.hs
@@ -0,0 +1,41 @@
+module Days.Day02 where
+
+import Data.List
+--import Text.ParserCombinators.ReadP
+import Data.List.Split
+
+
+data Policy = Policy { leftParam :: Int
+                     , rightParam :: Int
+                     , characterOfInterest :: Char
+                     } deriving Show
+
+parseLine :: String -> (Policy, String)
+parseLine s = (Policy{leftParam=read $ head asList,rightParam=read $ asList!!1, characterOfInterest = head $ asList!!2}, asList!!3)
+  where asList = filter (/= "") $ splitOneOf " -:" s
+--parseLine :: String -> readP (Policy, String)
+--parseLine s = (Policy{minOccur=1,maxOccur=2,characterOfInterest='a'}, "arf")
+--parse s = (Policy{minOccur=read lower,maxOccur=read upper,characterOfInterest=coi}, pw)
+--  where
+--    pw = tail $ dropWhile (/= ' ') afterUpper
+--    coi = head $ tail afterUpper
+--    (upper, afterUpper) = partition (== ' ') $ tail afterLower
+--    (lower, afterLower) = partition (== '-') s
+
+fulfillsPolicyA :: Policy -> String -> Bool
+fulfillsPolicyA policy candidatePassword = count >= leftParam policy && count <= rightParam policy
+  where
+    count = length $ filter (== characterOfInterest policy) candidatePassword
+
+fulfillsPolicyB :: Policy -> String -> Bool
+fulfillsPolicyB policy candidatePassword = length candidatePassword >= rightParam policy && aMatches /= bMatches
+  where
+    aMatches = candidatePassword!!(leftParam policy - 1) == characterOfInterest policy
+    bMatches = candidatePassword!!(rightParam policy - 1) == characterOfInterest policy
+
+
+day02a :: String -> String
+day02a = show . length . filter (uncurry fulfillsPolicyA) . map parseLine . lines
+
+day02b :: String -> String
+day02b = show . length . filter (uncurry fulfillsPolicyB) . map parseLine . lines
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,10 @@
 module Lib where
 
 import Days.Day01 (day01a, day01b)
+import Days.Day02 (day02a, day02b)
 
 
 daysMapping :: String -> (String -> String, String -> String)
 daysMapping "01" = (day01a, day01b)
+daysMapping "02" = (day02a, day02b)
 daysMapping _    = error "not implemented"
