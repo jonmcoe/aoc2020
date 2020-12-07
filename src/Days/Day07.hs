9cdd226e530010affeab235a9506d13552f3e3a5
Jon Coe <jonmcoe@gmail.com>
2020-12-07 13:49:12 -0500

    complete day07

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -8,6 +8,9 @@ type Requirement = [(Int, String)]
 myBag :: String
 myBag = "hinygoldbag"
 
+endBag :: String
+endBag = "nootherbag"
+
 parse :: String -> M.Map String Requirement
 parse = M.fromList . map parseLine . lines
 
@@ -15,18 +18,23 @@ parseLine :: String -> (String, Requirement)
 parseLine s = (outerBag, map toTuple innerBagList)
   where
     toTuple l = (read [head l] :: Int, tail l)
-    innerBagList = splitOn "," (halfSplit !! 1)
+    innerBagList = if innerBagString == endBag then [] else splitOn "," innerBagString
+    innerBagString = halfSplit !! 1
     outerBag = head halfSplit
     halfSplit = splitOn "contain" $ filter (`notElem` " s.") s
 
-ruleExpandedContents :: M.Map String Requirement -> String -> [String]
-ruleExpandedContents ruleMap start = start : concat [ruleExpandedContents ruleMap l | l <- map snd $ M.findWithDefault [] start ruleMap]
+ruleAgg :: M.Map String Requirement -> Int -> String -> Requirement
+ruleAgg ruleMap factor start = 
+  (factor, start) : 
+  concat [ruleAgg ruleMap (factor * fst l) (snd l) | l <- M.findWithDefault [] start ruleMap]
 
 day07a :: String -> String
-day07a s = show $ length $ filter (elem myBag) $ map (ruleExpandedContents ruleMap) ruleList
+day07a s = show $ length $ filter (elem myBag) $ map (map snd . ruleAgg ruleMap 1) ruleList
   where
     ruleMap = parse s
     ruleList = filter (/= myBag) $ M.keys $ parse s
 
 day07b :: String -> String
-day07b = const "b"
+day07b s = show $ sum (map fst $ ruleAgg ruleMap 1 myBag) - 1
+  where
+    ruleMap = parse s
