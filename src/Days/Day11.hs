d57bc78d2963ba8100a51af06ef9531f9058b171
Jon Coe <jonmcoe@gmail.com>
2020-12-11 08:10:08 -0500

    add day11 stub

---
+++ src/Days/Day11.hs
@@ -0,0 +1,7 @@
+module Days.Day11 where
+
+day11a :: String -> String
+day11a = const "a"
+
+day11b :: String -> String
+day11b = const "b"
--- src/Lib.hs
+++ src/Lib.hs
@@ -10,6 +10,7 @@ import Days.Day07 (day07a, day07b)
 import Days.Day08 (day08a, day08b)
 import Days.Day09 (day09a, day09b)
 import Days.Day10 (day10a, day10b)
+import Days.Day11 (day11a, day11b)
 
 daysMapping :: String -> (String -> String, String -> String)
 daysMapping "01" = (day01a, day01b)
@@ -22,4 +23,5 @@ daysMapping "07" = (day07a, day07b)
 daysMapping "08" = (day08a, day08b)
 daysMapping "09" = (day09a, day09b)
 daysMapping "10" = (day10a, day10b)
+daysMapping "11" = (day11a, day11b)
 daysMapping _    = error "not implemented"
