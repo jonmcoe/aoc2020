188c437c5819d32b1a80bc9e9197ca91ae22c350
Jon Coe <jonmcoe@gmail.com>
2020-12-12 01:50:23 -0500

    add day12b

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -42,8 +42,25 @@ executeInstruction ((x,y), bearing) (Forward mag)
   | bearing == Westy = ((x - mag, y), bearing)
 executeInstruction ((x,y), bearing) turn = ((x,y), rotate bearing turn)
 
+rotateWaypoint :: Position -> NavInstruction -> Position
+rotateWaypoint (x, y) (Leftwards 90) = (-1 * y, x)
+rotateWaypoint pos (Leftwards other) = rotateWaypoint (rotateWaypoint pos (Leftwards 90)) (Leftwards (other - 90))
+rotateWaypoint pos (Rightwards other) = rotateWaypoint pos (Leftwards (360 - other))
+
+
+executeInstruction2 :: (Position, Position) -> NavInstruction -> (Position, Position)
+executeInstruction2 (boatPos, (wayX, wayY)) (North mag) = (boatPos, (wayX, wayY + mag))
+executeInstruction2 (boatPos, (wayX, wayY)) (South mag) = (boatPos, (wayX, wayY - mag))
+executeInstruction2 (boatPos, (wayX, wayY)) (East mag) = (boatPos, (wayX + mag, wayY))
+executeInstruction2 (boatPos, (wayX, wayY)) (West mag) = (boatPos, (wayX - mag, wayY))
+executeInstruction2 ((boatX, boatY), (wayX, wayY)) (Forward mag) = ((boatX + wayX * mag, boatY + wayY * mag), (wayX, wayY))
+executeInstruction2 (boatPos, wayPos) turn = (boatPos, rotateWaypoint wayPos turn)
+
+
 day12a :: String -> String
 day12a = show . (\((x,y),_) -> abs x + abs y) . foldl executeInstruction ((0,0), Easty) . parseInstructions
 
 day12b :: String -> String
-day12b = const "b"
+day12b = show . (\((x,y),_) -> abs x + abs y) . foldl executeInstruction2 ((0,0), (10, 1)) . parseInstructions
+
+-- TODO: "dirtier" less typed solution as in  https://www.reddit.com/r/adventofcode/comments/kbj5me/2020_day_12_solutions/gfhv5la/ may be preferable
