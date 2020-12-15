b0bb793d9189aed01ec06ff853e9a408b10884e5
Jon Coe <jonmcoe@gmail.com>
2020-12-15 05:29:42 -0500

    add day14b

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -2,12 +2,11 @@ module Days.Day14 where
 
 import Data.Bits (clearBit, setBit)
 import Data.List (isPrefixOf)
-import Data.Maybe (catMaybes)
 import Days.Common (rightOrDie)
 import Text.Parsec
 import qualified Data.Map as M
 
-type MaskList = [(Int, Bool)]
+type MaskList = [(Int, Char)]
 data BinaryManipulatingInstruction = Mask MaskList | Mem Int Int deriving Show
 data MachineState = MachineState {
   entries :: M.Map Int Int,
@@ -20,13 +19,7 @@ parseMask :: Parsec String () BinaryManipulatingInstruction
 parseMask = do
   _ <- string "mask = "
   s <- many (oneOf "X01")
-  let
-    f i c = case c of
-      'X' -> Nothing
-      '0' -> Just (i, False)
-      '1' -> Just (i, True)
-      _ -> error "bad masky char"
-  return $ Mask $ catMaybes $ zipWith f [0..] (reverse s)
+  return $ Mask $ zip [0..] (reverse s)
 
 parseMem :: Parsec String () BinaryManipulatingInstruction
 parseMem = do
@@ -38,15 +31,25 @@ parseMem = do
 
 parseLine :: [Char] -> BinaryManipulatingInstruction
 parseLine l
-  | "mask" `isPrefixOf` l = rightOrDie $ parse parseMask "" l
+  | "mask" `isPrefixOf` l = rightOrDie $ parse parseMask "" l -- TODO: move manual prefix check into real parser attempts
   | "mem" `isPrefixOf` l = rightOrDie $ parse parseMem "" l
   | otherwise = error $ "bad line: " ++ l
 
-applyMask :: MaskList -> Int -> Int
-applyMask masky n = foldl (\acc (i,bitVal) -> alterBit bitVal acc i) n masky
+applyMaskSimple :: MaskList -> Int -> Int
+applyMaskSimple masky n = foldl (\acc (i,bitVal) -> alterBit bitVal acc i) n masky
   where
-    alterBit True = setBit
-    alterBit False = clearBit
+    alterBit '1' = setBit
+    alterBit '0' = clearBit
+    alterBit _ = const
+
+applyMaskWithFloaters :: MaskList -> Int -> [Int]
+applyMaskWithFloaters masky n = foldl makeList [n] masky
+  where
+    makeList acc (i, bitVal) = concatMap (f bitVal i) acc
+    f '1' i' n' = [setBit n' i']
+    f '0' _ n' = [n']
+    f 'X' i' n' = [setBit n' i', clearBit n' i']
+    f  c  _ _ = error $ "bad: "  ++ [c]
 
 --toBinary :: Int -> [Int] -> [Int]
 --toBinary 0 acc = acc
@@ -55,7 +58,13 @@ applyMask masky n = foldl (\acc (i,bitVal) -> alterBit bitVal acc i) n masky
 runInstruction1 :: MachineState -> BinaryManipulatingInstruction -> MachineState
 runInstruction1 mac (Mask m)  = mac {mask = m}
 runInstruction1 mac (Mem k v) = mac {entries = M.insert k newValue (entries mac)}
-  where newValue = applyMask (mask mac) v
+  where newValue = applyMaskSimple (mask mac) v
+
+runInstruction2 :: MachineState -> BinaryManipulatingInstruction -> MachineState
+runInstruction2 mac (Mask m)  = mac {mask = m}
+runInstruction2 mac (Mem k v) = mac {entries = M.union newEntries (entries mac)}
+  where newEntries = M.fromList [(k' , v) | k' <- applyMaskWithFloaters (mask mac) k]
+
 
 sumOfAllMemoryValuesAtCompletion :: (MachineState -> BinaryManipulatingInstruction -> MachineState) -> String -> Int
 sumOfAllMemoryValuesAtCompletion f = sum . M.elems . entries . foldl f emptyMachine . map parseLine . lines
@@ -64,4 +73,4 @@ day14a :: String -> String
 day14a = show . sumOfAllMemoryValuesAtCompletion runInstruction1
 
 day14b :: String -> String
-day14b = show . sumOfAllMemoryValuesAtCompletion runInstruction1
+day14b = show . sumOfAllMemoryValuesAtCompletion runInstruction2
