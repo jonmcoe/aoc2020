f1476f699d1551c7d7d5a6352094cc0767a2e420
Jon Coe <jonmcoe@gmail.com>
2020-12-16 20:58:39 -0500

    add day16b

--- src/Days/Day16.hs
+++ src/Days/Day16.hs
@@ -1,15 +1,14 @@
 module Days.Day16 where
 
 import Days.Common (rightOrDie)
-import Data.Maybe (fromJust)
+import Data.List (isPrefixOf, transpose)
 import Text.Parsec
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-type RulesMap = M.Map String (S.Set Int)
 data FullInput =
   FullInput
-  { rules :: RulesMap
+  { rules :: M.Map String (S.Set Int)
   , myTicket :: [Int]
   , nearbyTickets :: [[Int]]
   } deriving Show
@@ -23,10 +22,11 @@ parseAll = do
   skipMany1 endOfLine
   _ <- string "nearby tickets:\n"
   nearbyTicketsIn <- parseIntList `sepEndBy1` endOfLine
-  return FullInput {
-    rules = M.fromList rulesList,
-    myTicket=myTicketIn,
-    nearbyTickets=nearbyTicketsIn}
+  return FullInput
+         { rules = M.fromList rulesList
+         , myTicket=myTicketIn
+         , nearbyTickets=nearbyTicketsIn 
+         }
   where
     parseIntList = do
       l <- many1 digit `sepBy1` char ','
@@ -54,13 +54,23 @@ day16a s = show $ sum invalidFields
     fullAcceptable = S.unions $ M.elems $ rules fullInput
     fullInput = rightOrDie $ parse parseAll "" s
 
-day16b :: String -> String
-day16b s = show validNearbyTickets
+prune :: (Eq a, Ord a) => [S.Set a] -> [S.Set a]
+prune l = map removeElements l
   where
---    legend = map fromJust $
---      [find (\col -> all (fieldName -> S.member col rulesMap M.!  )
---    rulesMap = rules fullInput
---    cols = [(i, [t!!i | t <- validNearbyTickets]) | i <- [0.. (length (head validNearbyTickets) - 1)]]
+    removeElements s = if S.size s > 1 then S.difference s singletons else s
+    singletons = S.unions $ filter (\s -> S.size s == 1) l
+
+day16b :: String -> String
+day16b s = show $ product $ map (\k -> myTicket fullInput !! k) eligible
+  where
+    eligible = map fst $ filter (\(_,name) -> "depart" `isPrefixOf` name) (zip [0 .. ] legend)
+    legend = map (S.elemAt 0) $ until (all (\ s' -> S.size s' == 1)) prune candidates
+    candidates = map
+                   (S.fromList . map snd . filter (\ (c, k) -> allFulfill k c))
+                   ([[(col, key) | key <- M.keys rulesMap] | col <- cols])
+    allFulfill key col = all (\n -> S.member n (rulesMap M.! key)) col
+    rulesMap = rules fullInput
+    cols = transpose validNearbyTickets
     validNearbyTickets = filter (all (`S.member` fullAcceptable)) (nearbyTickets fullInput)
     fullAcceptable = S.unions $ M.elems $ rules fullInput
     fullInput = rightOrDie $ parse parseAll "" s
